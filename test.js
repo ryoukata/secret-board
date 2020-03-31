4392a37e77d2751cddba5954f8d2cd877fe32326
ryoukata <ryoya030528@gmail.com>
2020-03-31 20:53:14 +0900

    add to protect from XSS

---
+++ test.js
@@ -0,0 +1,34 @@
+'use strict';
+const pug = require('pug');
+const assert = require('assert');
+
+// pug のテンプレートにおける XSS 脆弱性のテスト
+const html = pug.renderFile('./views/posts.pug', {
+  posts: [{
+    id: 1,
+    content: '<script>alert(\'test\');</script>',
+    postedBy: 'guest1',
+    trackingCookie: 1,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }],
+  user: 'guest1'
+});
+
+// pug のテンプレートにおける スペースのテスト
+const html2 = pug.renderFile('./views/posts.pug', {
+  posts: [{
+    id: 2,
+    content: ' ',
+    postedBy: 'guest1',
+    trackingCookie: 2,
+    createdAt: new Date(),
+    updatedAt: new Date()
+  }],
+  user: 'guest1'
+});
+
+// スクリプトタグがエスケープされて含まれていることをチェック
+assert(html.includes('&lt;script&gt;alert(\'test\');&lt;/script&gt;'));
+assert(html2.includes(' '));
+console.log('テストが正常に完了しました');
